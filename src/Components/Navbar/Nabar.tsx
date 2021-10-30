5ce642eeefca4c57a5d3bb5d26be029fa1135eaf
Osiroski <osiroskinoelski@gmail.com>
2021-10-30 23:27:07 +0300

    Fixed Navbar

--- src/Components/Navbar/Nabar.tsx
+++ src/Components/Navbar/Nabar.tsx
@@ -1,14 +1,12 @@
-import { Container, Nav, NavItem,Image } from 'react-bootstrap'
-import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import { Container, Navbar, NavItem,Image, Nav, Dropdown } from 'react-bootstrap'
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
-import { useSelector } from 'react-redux';
-import bamboo from '../../assets/bamboo.jpg'
-
-
 import { NavLink } from 'react-router-dom';
+import bamboo from '../../assets/bamboo.jpg'
+import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store/store';
 import './Navbar.css'
+
 
 const Navigation = () => {
     const state = useSelector((state:RootState) => state.user)
@@ -17,42 +15,42 @@ const Navigation = () => {
         window.location.href = '/';
     }
     return (
-        <Nav className="navbar navbar-expand-lg navbar-light bg-light rounded-3 navshadow">
+        <Navbar expand="lg" bg="light" className="rounded-3 navshadow">
             <Container fluid>
-                <a className="navbar-brand" href="/">Recyski</a>
+                <Navbar.Brand href="/">Recyski</Navbar.Brand>
                 <NavbarToggle className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </NavbarToggle>
-                <NavbarCollapse className="collapse navbar-collapse bg-light" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto ml-auto mb-2 mb-lg-0">
+                <NavbarCollapse className="collapse bg-light" id="navbarSupportedContent">
+                    <Nav className="me-auto ml-auto mb-2 mb-lg-0 justify-contnet-center">
                         <NavItem className=" mr-2">
                             <NavLink className="btn active" aria-current="page" to="/" >Home</NavLink>
                         </NavItem>
-                        <NavItem className="dropdown mr-2">
-                            <DropdownToggle className="nav-link" variant="outline-sccuess" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="true">
+                        <Dropdown className="dropdown mr-2">
+                            <Dropdown.Toggle className="nav-link" variant="outline-sccuess" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="true">
                                 Learn More
-                            </DropdownToggle>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><h6 className="dropdown-header">Interests</h6></li>
-                                <li><hr className="dropdown-divider divider" /></li>
-                                <li><a className="dropdown-item fs-m" href="/about" >About Us</a></li>
-                                <li><a className="dropdown-item fs-m" href="/">Another action</a></li>
-                                <li><a className="dropdown-item fs-m" href="/">Something else here</a></li>
-                            </ul>
-                        </NavItem>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu align={'start'} aria-labelledby="navbarDropdown">
+                                <Dropdown.Header>Interests</Dropdown.Header>
+                                <Dropdown.Divider className=" divider"></Dropdown.Divider>
+                                <Dropdown.Item className=" fs-m" href="/about">About Us</Dropdown.Item>
+                                <Dropdown.Item className=" fs-m" href="/">Another action</Dropdown.Item>
+                                <Dropdown.Item className=" fs-m" href="/">Something else here</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                         <NavItem className="mr-2">
                             <NavLink className="btn" to="/" tabIndex={-1}>Get Started</NavLink>
                             
                         </NavItem>
-                    </ul>
+                    </Nav>
                     {state.isLoggedin ?
-                        <div className="d-flex mb-2">
+                        <div className="d-flex mb-2 justify-content-center">
                             <Image alt="" className='mr-2' src={bamboo} roundedCircle width={'70'} height={'70'} />
                             <NavLink className="btn btn-lg btn-outline-info me-2" to="/login" onClick={logout}>Log out</NavLink>
                             
                         </div>
                         :
-                        <div className="d-flex mb-2">
+                        <div className="d-flex mb-2 justify-content-center">
                             <NavLink className="btn btn-outline-info me-2" to="/login" >Sign in</NavLink>
                             <NavLink className="btn btn-outline-success me-2 fw-2" to="/register">Register</NavLink>
                         </div>
@@ -60,7 +58,7 @@ const Navigation = () => {
                     }
                 </NavbarCollapse>
             </Container>
-        </Nav>
+        </Navbar>
     )
 }
 
