42fe6c9841e2e8f7e843372ef264ccc88357224e
Osiroski <osiroskinoelski@gmail.com>
2021-10-31 22:46:09 +0300

    Fixed Brands

--- src/Components/About/brands.tsx
+++ src/Components/About/brands.tsx
@@ -1,4 +1,5 @@
 
+import { Col, Container, Row, Image } from 'react-bootstrap'
 import visa from '../../assets/1.svg'
 import coke from '../../assets/2.svg'
 import sony from '../../assets/3.svg'
@@ -6,29 +7,29 @@ import sony from '../../assets/3.svg'
 
 const Brands = () => {
     return (
-        <div className="container-fluid">
-            <div className="row bg-light rounded-3" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-                <div className="col">
-                <div className="row">
-                    <h2 className="display-4 text-center">
-                        Our Partners
-                    </h2>
-                </div>
-                <div className="row row-cols-1 row-cols-md-2 g-3">
-                    <div id='brand1' className="col my-3">
-                        <img src={visa} alt="coke" width='100%' height='100%'/>
-                    </div>
-                    <div id='brand2' className="col my-3">
-                        <img src={coke} alt="coke" width='100%' height='100%'/>
-                    </div>
-                    <div id='brand3' className="col my-3">
-                        <img src={sony} alt="coke" width='100%' height='100%'/>
-                    </div>
-                </div>
-                </div>
-            </div>
+        <Container fluid>
+            <Row className="bg-light rounded-3" data-aos="flip-left" data-aos-easing="ease-out-cubic">
+                <Col>
+                    <Row>
+                        <h2 className="display-4 text-center">
+                            Our Partners
+                        </h2>
+                    </Row>
+                    <Row className="row-cols-1 row-cols-md-2 g-3">
+                        <Col id='brand1' className="my-3">
+                            <Image src={visa} alt="coke" width='100%' height='100%' />
+                        </Col>
+                        <Col id='brand2' className="my-3">
+                            <Image src={coke} alt="coke" width='100%' height='100%' />
+                        </Col>
+                        <Col id='brand3' className="my-3">
+                            <Image src={sony} alt="coke" width='100%' height='100%' />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
 
-        </div>
+        </Container>
     )
 }
 
